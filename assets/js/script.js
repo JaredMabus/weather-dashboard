b58b2f9667570acc149e91ad83d167f8c7726752
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-09-27 23:01:50 -0500

    added local storage and refactored JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,55 @@ const container = $("#page-layout");
 const searchContainer = $("#search-container");
 const weatherContainer = $("#weather-container");
 const fiveDayForecastEl = $("#five-day-forecast");
+const todayWeatherEl = $("#today-weather");
 // Search
 const searchForm = $("#search-form");
 const searchInputEl = $("#search-input");
 const searchHistoryList = $("#history-list");
 
-/* Weather API Log and Lat
-    url: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=d40a97aa8657a190a859f3f282eef526
-    url: https://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid=d40a97aa8657a190a859f3f282eef526
-*/
-
 const apiKey = "d40a97aa8657a190a859f3f282eef526";
-const searchHistory = [];
-let currDay = null;
+let currDay = moment().format("DDD");
+today = moment().format("DDD");
 const weatherDays = [];
+const searchHistory = [];
+let todaysWeather;
 
 // Fetch api call to get lon and lat of city
 const getWeatherForecast = (city) => {
   const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
   fetch(requestUrl)
     .then((response) => {
-      // console.log(response);
       return response.json();
     })
     .then((data) => {
-      // console.log(data.city.coord);
       const { city } = data;
       addCityToHistory(city);
       return city.coord;
     })
     .then((coord) => {
       const { lat, lon } = coord;
-      const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-      return fetch(requestUrl);
+      return forecastLatLonApi(lat, lon);
     })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// API Lat and Lon
+const forecastLatLonApi = (lat, lon) => {
+  const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  console.log(lat, lon);
+  return fetch(requestUrl)
     .then((response) => {
-      // console.log(response);
       return response.json();
     })
     .then((sampleData) => {
+      // Set today's weather
+      todaysWeather = [sampleData.city, sampleData.list[0]];
+
+      // Clear weather array
+      weatherDays.length = 0;
+
       sampleData.list.forEach(function (tsObj) {
         // Makes a moment date object for each record
         const dateObj = moment.unix(tsObj.dt);
@@ -51,12 +61,18 @@ const getWeatherForecast = (city) => {
 
         // If the current date in tsObj hasn't had a record put into weatherDays, do that now
         // Then skip over all other records for this day
-        if (dateNum !== currDay && weatherDays.length < 5) {
+        if (
+          dateNum !== currDay &&
+          today !== dateNum &&
+          weatherDays.length < 5
+        ) {
           weatherDays.push(tsObj);
           currDay = dateNum;
-          // console.log(tsObj);
         }
       });
+    })
+    .then(() => {
+      renderTodayWeather();
       renderForecast();
     })
     .catch((err) => {
@@ -73,7 +89,9 @@ const renderElement = (tag, content, appendTo) => {
 
 // Render 5-day forecast card
 const renderForecast = () => {
+  console.log(weatherDays);
   fiveDayForecastEl.html("");
+
   weatherDays.forEach((day) => {
     const { dt, main, wind } = day;
     const weatherIcon = day.weather[0].icon;
@@ -106,43 +124,94 @@ const renderForecast = () => {
   });
 };
 
+// Render today's weather
+const renderTodayWeather = () => {
+  todayWeatherEl.html("");
+  const cityInfo = todaysWeather[0];
+  const weather = todaysWeather[1];
+
+  const { dt, main, wind } = weather;
+  const weatherIcon = weather.weather[0].icon;
+  // Create Elements
+  const cityContainer = $("<div class='city-container'>");
+  const cityEl = $("<h3>");
+  const dateEl = $("<h4>");
+  const tempEl = $("<p>");
+  const windEl = $("<p>");
+  const humidityEl = $("<p>");
+  const weatherIconEl = $("<img class='card-text'>");
+  weatherIconEl.attr(
+    "src",
+    `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+  );
+
+  cityEl.text(cityInfo.name);
+  dateEl.text(moment.unix(dt).format("M/D/YYYY"));
+  tempEl.text(`Temp: ${main.temp} (\u00B0F)`);
+  windEl.text(`Wind: ${wind.speed} MPH`);
+  humidityEl.text(`Humidity: ${main.humidity}%`);
+
+  // Append Elements
+  todayWeatherEl.append(cityContainer);
+  cityContainer.append(cityEl);
+  cityContainer.append(dateEl);
+  cityContainer.append(weatherIconEl);
+
+  todayWeatherEl.append(tempEl);
+  todayWeatherEl.append(windEl);
+  todayWeatherEl.append(humidityEl);
+};
+
 // Render search history
 const renderSearchHisotry = () => {
   searchHistoryList.html("");
-  searchHistory.forEach((item) => {
-    const newHistoryItem = $("<button 'type:button'>");
-    newHistoryItem.addClass("list-group-item list-group-item-action");
-    newHistoryItem.text(item.name);
-    searchHistoryList.append(newHistoryItem);
+  searchHistory.forEach((item, i) => {
+    searchHistoryList.append(
+      $("<button>")
+        .addClass("list-group-item list-group-item-action")
+        .text(item.name)
+        .attr("id", "history-item")
+        .attr("value", item.name)
+        .attr("data-lat", item.lat)
+        .attr("data-lon", item.lon)
+    );
   });
 };
 
 // Add city to array and update localstorage
 const addCityToHistory = (city) => {
-  // Used .some to test if already in history
-  // url: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+  // Used .some to test if already in history. url: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
   const alreadyInHistory = (item) => city.name === item.name;
-  console.log(searchHistory.some(alreadyInHistory));
   if (searchHistory.some(alreadyInHistory)) {
     return;
   } else {
-    searchHistory.push({
+    searchHistory.unshift({
       name: city.name,
       lat: city.coord.lat,
       lon: city.coord.lon,
     });
     renderSearchHisotry();
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
   }
-  // update localstorage
 };
 
 // Create local storage functions
+const getLocalStorage = () => {
+  const searchHistoryLocal = JSON.parse(localStorage.getItem("searchHistory"));
+  if (searchHistoryLocal !== null) {
+    searchHistoryLocal.forEach((item) => {
+      searchHistory.push(item);
+    });
+  } else {
+    return;
+  }
+};
 
 // Run init at start
 const init = function () {
+  getLocalStorage();
   renderSearchHisotry();
 };
-
 init();
 
 // Event listeners
@@ -150,10 +219,17 @@ init();
 searchForm.on("submit", (e) => {
   e.preventDefault();
   const searchInput = searchInputEl.val();
-
   if (searchInput && searchInput !== null) {
     getWeatherForecast(searchInput);
   } else {
     console.log("Empty search field");
   }
+});
+
+// Add event listener on search container to run another query
+searchContainer.on("click", "#history-item", function () {
+  console.log($(this).val());
+  const lat = $(this).attr("data-lat");
+  const lon = $(this).attr("data-lon");
+  forecastLatLonApi(lat, lon);
 });
